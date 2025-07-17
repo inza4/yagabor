4e9595a4969371f7c4efa3164361fef5284ec8f0
inza4 <167521670+inza4@users.noreply.github.com>
2025-07-17 16:22:31 +0200

    deploy fix

--- web-ui/deploy/build1.sh
+++ web-ui/deploy/build1.sh
@@ -2,4 +2,4 @@ echo "Installing Rustup..."
 # Install Rustup (compiler)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 # Adding binaries to path
-source "/root/.cargo/env"
+source ".cargo/env"
