d2d67fc26f888b61ffd35f09655a3cb26d901887
inza4 <167521670+inza4@users.noreply.github.com>
2025-07-17 16:22:31 +0200

    build script

--- web-ui/deploy/build1.sh
+++
@@ -1,5 +0,0 @@
-echo "Installing Rustup..."
-# Install Rustup (compiler)
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-# Adding binaries to path
-source ".cargo/env"
--- web-ui/deploy/build2.sh
+++
@@ -1,3 +0,0 @@
-echo "Installing wasm-pack..."
-# Install wasm-pack
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
